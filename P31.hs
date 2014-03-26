75762556698f2e36886e82444ed0a01b6a0d6554
Tosh Shimayama <2sm@csc.jp>
2014-03-26 15:18:37 +0900

    fix P31

--- P31.hs
+++ P31.hs
@@ -1,14 +1,27 @@
 module P31 where
-
-
 import Data.Array
 
-kinds = [1,2,5,10,20,50,100,200]
+-- |
+-- >>> ways 5 1
+-- 3
+--
+-- >>> ways 6 1
+-- 4
+--
+-- >>> ways 5 2
+-- 4
+--
+-- >>> ways 200 $ (length coins) -1
 
--- ways to construct value t by using k or less kinds.
-ways t k tmax = r !! (t, k) where
-  array ((0,0),(tmax,length kinds)) [((x,y),w x y) | x<-[0..tmax], y<-[0..length kinds]]
-  w 0 _ = 0
-  w _ 0 = 0
-  w x y = 
+coins :: [Int]
+coins = [1,2,5,10,20,50,100,200]
+
+-- number of ways to make total value t by using k or less kinds.
+ways :: Int -> Int -> Int
+ways tot ci = r ! (tot, ci) where
+  r = array ((0,0),(tot+1,ci+1)) [((t,k),w t k) | t<-[0..tot], k<-[0..ci]]
+  w 0 _ = 1
+  w _ 0 = 1
+  w t k | t < 0      = 0
+        | otherwise  = r ! (t, k-1) + w (t - coins !! k) k
 
