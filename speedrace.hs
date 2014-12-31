515f3ddda21df4515c8304a766eecf5461bd5fc7
Tosh Satake <2sm@csc.jp>
2014-12-31 05:50:29 +0000

    add race

--- speedrace.hs
+++ speedrace.hs
@@ -1,25 +1,26 @@
 import System.Environment (getArgs)
+import Data.Function
+import Data.List
+import Text.Printf
 -- import Debug.Trace
 -- dbg :: (Show a) => a -> a
 -- dbg x = trace (show x) x
 
-type Track = [(Float, Int)]
+type Track = [(Double, Int)]
 type Angle = Int
-type Acc = Float
-type Velocity = Float
-type Distance = Float
-type Time = Float
-data Car = Car { carId :: Int,
-                 top :: Velocity,
-                 acc :: Acc,
-                 dec :: Acc }  deriving (Show)
+type Velocity = Double
+type Distance = Double
+type Time = Double
+data Car = Car { top :: Velocity,
+                 tacc :: Time,
+                 tdec :: Time }  deriving (Show)
 
 -- |
 -- >>> head $ fst $ parse "1.029 115 1.122 125 1.185 100 0.53 110 0.751 95 1.242 85 0.533 85 1.003 120 0.465 110 0.546 125 0.446 90 0.582 70 0.878 45 0.49 30 1.016 130 1.047 140 1.146 75 0.496 85 0.857 125 0.971 0\n1 266 8.1 1.4"
 -- (1.029,115)
 --
 -- >>> head $ snd $ parse "1.029 115 1.122 125 1.185 100 0.53 110 0.751 95 1.242 85 0.533 85 1.003 120 0.465 110 0.546 125 0.446 90 0.582 70 0.878 45 0.49 30 1.016 130 1.047 140 1.146 75 0.496 85 0.857 125 0.971 0\n1 266 8.1 1.4"
--- Car {carId = 1, top = 266.0, acc = 8.1, dec = 1.4}
+-- Car {top = 266.0, tacc = 8.1, tdec = 1.4}
 --
 parse :: String -> (Track, [Car])
 parse str = (spl $ words str0, cars) where
@@ -27,35 +28,33 @@ parse str = (spl $ words str0, cars) where
   spl (x0:x1:xs) = (read x0 :: Distance, read x1 :: Angle) : spl xs
   spl _ = []
   cars = map (parsecar . words) strs
-  parsecar [x0,x1,x2,x3] = Car (read x0 :: Int) (read x1 :: Velocity) (read x2 :: Acc) (read x3 :: Acc)
-  parsecar _ = Car 0 0 0 0
+  parsecar [_,x1,x2,x3] = Car (read x1 :: Velocity) (read x2 :: Time) (read x3 :: Time)
+  parsecar _ = Car 0 0 0
 
 -- |
--- >>> run1 (Car 1 10.0 1.0 1.0) 10.0 100.0 0
--- (10.0,10.0)
+-- >>> run1 (Car 10.0 1.0 1.0) 10.0 10.0 0
+-- (3600.0,10.0)
 --
--- >>> run1 (Car 1 10.0 1.0 1.0) 0.0 50.0 0
--- (10.0,10.0)
+-- >>> run1 (Car 10.0 1.0 1.0) 10.0 1.0 0
+-- (360.0,10.0)
 --
--- >>> run1 (Car 1 10.0 1.0 1.0) 10.0 50.0 180
--- (10.0,0.0)
---
-run1 :: Car -> Velocity -> Distance -> Angle -> (Time, Velocity)
-run1 (Car _ vm a d) v0 l ang1 = (t0 + tf + t1, v1) where
-  t0 = (vm - v0) / a
-  tf = lf / vm
-  t1 = (vm - v1) / d
+run1 :: Car       -- spec
+     -> Velocity  -- initial value
+     -> Distance -> Angle -> (Time, Velocity)
+run1 (Car vm ta td) v0 l ang1 = (t0 + tf + t1, v1) where
+  t0 = ta * (vm - v0) / vm
+  t1 = td * (vm - v1) / vm
+  tf = lf / (vm / 3600.0)
   v1 = vm * (1.0 - fromIntegral ang1 / 180.0)
-  lf = l - 0.5 * (vm + v0) * t0 - 0.5 * (vm + v1) * t1
+  lf = l
+     - 0.5 * (vm + v0) / 3600.0 * t0
+     - 0.5 * (vm + v1) / 3600.0 * t1
   
 track :: Track
 (track, _:_) = parse "1.029 115 1.122 125 1.185 100 0.53 110 0.751 95 1.242 85 0.533 85 1.003 120 0.465 110 0.546 125 0.446 90 0.582 70 0.878 45 0.49 30 1.016 130 1.047 140 1.146 75 0.496 85 0.857 125 0.971 0\n1 266 8.1 1.4"
 
 -- |
--- >>> run (Car 1 10.0 1.0 1.0) [(50.0,0),(50.0,180)]
--- 20.0
---
--- >>> run (Car 10 266 4.0 1.9) track
+-- >>> printf "%0.2f" $ run (Car 266 4.0 1.9) track
 -- 241.05
 --
 run :: Car -> Track -> Time
@@ -69,5 +68,8 @@ main :: IO ()
 main = do
   [filename] <- getArgs
   doc <- readFile filename
-  mapM_ putStrLn $ lines doc
+  mapM_ putStrLn $ fmt $ parse doc where
+    fmt (trk, cars) = map (\(i,t) -> printf "%d %0.2f" i t) laps' where
+      laps = map (\c -> run c trk) cars
+      laps' = sortBy (compare `Data.Function.on` snd) $ zip [(1::Int)..] laps
 
