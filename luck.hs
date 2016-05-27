c24dda991e452b3f78afac5748d66d9c83d2b6d9
Tosh Satake <2sm@csc.jp>
2016-05-27 08:15:51 -0700

    add type alias

--- luck.hs
+++ luck.hs
@@ -16,22 +16,23 @@ m n = s where
   n' = n `div` 2
   s = sum [ l' n' i ^ (2::Integer) | i <- [0..9*n'] ]
 
-
--- l :: Int -> Int -> Integer
--- l = fix f
+type Ftype = Int -> Int -> Integer
 
 -- | open recursion pattern
-f :: (Int -> Int -> Integer) -> Int -> Int -> Integer
+f :: Ftype -> Ftype
 f _ _ 0 = 1
 f _ n 1 = toInteger n
 f _ 1 k = if 0 <= k && k <= 9 then 1 else 0
 f mf n k = sum [mf (n-1) j | j <- [0 `max` (k-9)..k]]
+-- l :: Int -> Int -> Integer
+-- l = fix f
+
+-- | top-level memoized function
+l' :: Ftype
+l' n k = llist !! n !! k
 
 llist :: [[Integer]]
 llist = map (\n -> map (f l' n) [0..]) [0..] 
-
-l' :: Int -> Int -> Integer
-l' n k = llist !! n !! k
 
 -- | entry point
 main :: IO ()
