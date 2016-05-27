599d3a1510c132f73ca9a80fbbecf8fecf039403
Tosh Satake <2sm@csc.jp>
2016-05-27 07:57:55 -0700

    use open recursion / CAF

--- luck.hs
+++ luck.hs
@@ -1,27 +1,37 @@
 import System.Environment (getArgs)
+--import Data.Function (fix)
 
 -- | 
--- >>> m 12
+-- >>> m 2
+-- 10
+--
+-- >>> m 4
 -- 670
+--
+-- >>> m 10
+-- 432457640
 --
 m :: Int -> Integer
 m n = s where
   n' = n `div` 2
-  s = sum [ l n' i ^ 2 | i <- [0..9*n'] ]
+  s = sum [ l' n' i ^ (2::Integer) | i <- [0..9*n'] ]
 
--- ll :: Int -> Int -> Integer
--- ll _ 0 = 1
--- ll n 1 = toInteger n
--- ll 1 k = if 0 <= k && k <= 9 then 1 else 0
--- ll n k = sum [ll (n-1) j | j <- [0 `max` (k-9)..k]]
 
-l :: Int -> Int -> Integer
-l p q = map (\n -> map (l' n) [0..]) [0..] !! p !! q
-  where
-    l' _ 0 = 1
-    l' n 1 = toInteger n
-    l' 1 k = if 0 <= k && k <= 9 then 1 else 0
-    l' n k = sum [l (n-1) j | j <- [0 `max` (k-9)..k]]
+-- l :: Int -> Int -> Integer
+-- l = fix f
+
+-- | open recursion pattern
+f :: (Int -> Int -> Integer) -> Int -> Int -> Integer
+f _ _ 0 = 1
+f _ n 1 = toInteger n
+f _ 1 k = if 0 <= k && k <= 9 then 1 else 0
+f mf n k = sum [mf (n-1) j | j <- [0 `max` (k-9)..k]]
+
+llist :: [[Integer]]
+llist = map (\n -> map (f l' n) [0..]) [0..] 
+
+l' :: Int -> Int -> Integer
+l' n k = llist !! n !! k
 
 -- | entry point
 main :: IO ()
