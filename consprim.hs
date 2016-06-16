0c0145596288f779080efe1ac9ac0b44e85e6a46
Tosh Satake <2sm@csc.jp>
2016-06-16 16:47:51 +0900

    solved..

--- consprim.hs
+++ consprim.hs
@@ -1,6 +1,6 @@
 import System.Environment (getArgs)
 --import Data.List (partition, permutations)
---import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe)
 --import Debug.Trace (trace)
 --import Control.Applicative (liftA2)
 
@@ -25,33 +25,33 @@ primes = 2 : sieve [3,5..] where
 -- >>> perm 4
 -- [[1,2,3,4],[1,4,3,2]]
 --
+-- >>> perm 6
+-- [[1,4,3,2,5,6],[1,6,5,2,3,4]]
+--
 perm :: Int -> [[Int]]
-perm n = map (1:) $ perm' xe xo where
-  (xe,xo) = ([2,4..n], [3,5..n])
-  perm' [] xs = [xs]
-  perm' xs [] = [xs]
-  perm' xs xs' = 
-   let
-     sublist x = map (x:) $ perm' xs' (except x)
-     except x = l ++ r where (l,_:r) = break (==x) xs
-   in foldl1 (++) $ map sublist xs
+perm n = perm' 1 [2,4..n] [3,5..n] where
+  perm' x [] [] = if isPrime (1+x) then [[x]] else []
+  perm' x ys zs = foldl (++) [] $ mapMaybe sublist ys where
+    sublist y = if isPrime (x+y)
+                then Just $ map (x:) $ perm' y zs (except y ys)
+                else Nothing
+  except y ys = l ++ r where (l,_:r) = break (==y) ys
 
 -- | obviously n has to be odd in order the answer to be non-zero.
 -- >>> all (==0) $ map numconspr [1,3,5,7,9,11,13,15,17,18,19,20]
--- True 
+-- True
 --
--- >>> map numconspr [2,4,6,8]
--- [1,2,2,4]
+-- >>> map numconspr [2,4,6,8,10,12,14]
+-- [1,2,2,4,96,1024,2880]
 --
 -- >>> numconspr 12
 -- 1024
 --
 numconspr :: Int -> Int
-numconspr n
-  | odd n     = 0
-  | n > 17    = 0
-  | otherwise = length $ filter check (perm n)
-    where check xs = all isPrime $ zipWith (+) xs (last xs : xs)
+numconspr n =
+  if odd n
+  then 0
+  else length $ perm n
 
 -- | entry point
 main :: IO ()
@@ -60,5 +60,4 @@ main = do
   doc <- readFile filename
   mapM_ proc $ lines doc where
     proc = print . numconspr . (read :: String -> Int)
-
 
